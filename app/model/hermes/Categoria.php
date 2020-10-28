d6f38f1984971d4d8240d5b3bfc7f49de3c56115
Andre Scherrer <device2code@gmail.com>
2020-10-28 01:44:23 -0300

    inicio

---
+++ app/model/hermes/Categoria.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Categoria Active Record
+ * @author  André C. Scherrer
+ */
+class Categoria extends TRecord
+{
+    const TABLENAME = 'categoria';
+    const PRIMARYKEY= 'id';
+    const IDPOLICY =  'max'; // {max, serial}
+    
+    
+    /**
+     * Constructor method
+     */
+    public function __construct($id = NULL, $callObjectLoad = TRUE)
+    {
+        parent::__construct($id, $callObjectLoad);
+        parent::addAttribute('nome');
+    }
+
+
+}
